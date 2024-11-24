db81a35bdba560ccbce5561d4d7275888436a6e5
akash ahmed <hafejmohammadakash@gmail..com>
2024-11-25 00:11:42 +0600

    finnised

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 const users = [
     {id: 1, name: "AKash", email: "akash@gmail.com"},
@@ -21,6 +22,10 @@ app.get("/users", (req, res) => {
 app.post('/users', (req, res) => {
     console.log("Post Api Hitting")
     console.log(req.body)
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
 })
 
 
